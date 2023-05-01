3df781ca0437ee694ada7625454761f96a5fef30
marlisonmourao <bentesmourao@gmail.com>
2023-05-01 02:32:41 -0400

    home

--- dayli-diet/src/screens/Home/styles.ts
+++ dayli-diet/src/screens/Home/styles.ts
@@ -1,7 +1,10 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.gray_700};
   padding: 0 24px;
+
+  margin-top: ${getStatusBarHeight() + 36}px;
 `
