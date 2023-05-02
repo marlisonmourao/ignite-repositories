e796fbc25901bd906cda4e868680ca2683556ba9
marlisonmourao <bentesmourao@gmail.com>
2023-05-01 21:57:03 -0400

    screen home

--- dayli-diet-rn/src/screens/Home/index.tsx
+++ dayli-diet-rn/src/screens/Home/index.tsx
@@ -1,10 +1,58 @@
+import { useState } from 'react'
+import { Button } from '@components/Button'
+import { Container, HeaderList, Label } from './styles'
+
 import { Header } from '@components/Header'
-import { Container } from './styles'
+import { PorcentCard } from '@components/PorcentCard'
+import { DayCard } from '@components/DayCard'
+import { HistoryByDayDTO } from '@dtos/historyByDayDTO'
+import { SectionList } from 'react-native'
 
 export function Home() {
+  const [history, setHistory] = useState<HistoryByDayDTO[]>([
+    {
+      title: '12.08.22',
+      data: [
+        {
+          id: '1',
+          food: 'Hamburguer',
+          hours: '10:43',
+          status: true,
+        },
+      ],
+    },
+    {
+      title: '16.11.22',
+      data: [
+        {
+          id: '2',
+          food: 'Batata-Frita',
+          hours: '15:43',
+          status: false,
+        },
+      ],
+    },
+  ])
+
   return (
     <Container>
       <Header />
+      <PorcentCard porcent="98,9" success={true} />
+
+      <Label>Refeições</Label>
+
+      <Button variant="primary" title="Nova refeição" />
+
+      <SectionList
+        sections={history}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <DayCard data={item} />}
+        renderSectionHeader={({ section }) => (
+          <HeaderList>{section.title}</HeaderList>
+        )}
+        contentContainerStyle={{ paddingBottom: 100, paddingTop: 32 }}
+        showsVerticalScrollIndicator={false}
+      />
     </Container>
   )
 }
--- dayli-diet-rn/src/screens/Home/styles.ts
+++ dayli-diet-rn/src/screens/Home/styles.ts
@@ -1,5 +1,5 @@
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
@@ -7,4 +7,24 @@ export const Container = styled.View`
   padding: 0 24px;
 
   margin-top: ${getStatusBarHeight() + 36}px;
+`
+
+export const Label = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.font_size.md}px;
+    color: ${theme.colors.gray_100};
+    font-family: ${theme.font_family.regular};
+  `}
+
+  margin: 40px 0 8px
+`
+
+export const HeaderList = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.font_size.lg}px;
+    color: ${theme.colors.gray_100};
+    font-family: ${theme.font_family.bold};
+  `}
+
+  margin-bottom: 8px
 `
