b2b1ba85ddd3a7011a8f99026462e6e399c68cd9
marlisonmourao <bentesmourao@gmail.com>
2023-05-03 21:03:46 -0400

    routes application

---
+++ dayli-diet-rn/src/routes/app.stack.routes.tsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { Home } from '@screens/Home'
+import { NewMeal } from '@screens/NewMeal'
+import { Statistics } from '@screens/Statistics'
+import { Feedback } from '@screens/Feedback'
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export function AppStackRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="home" component={Home} />
+      <Screen name="new" component={NewMeal} />
+      <Screen name="statistics" component={Statistics} />
+      <Screen name="feedback" component={Feedback} />
+    </Navigator>
+  )
+}
